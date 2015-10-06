1fb2b04cc77a8d0617cd8f11fe985f91d9b3bd82
root <root@localhost.localdomain>
2015-10-06 22:20:14 +0000

    blog adding done

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,4 +10,31 @@ class BlogsController <ApplicationController
 
   end
 
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.user = User.find(2)
+
+    if @blog.save
+
+      flash[:success] = 'Your blog was  created successfully'
+      redirect_to blogs_path
+
+
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:blogname,:description)
+  end
+
+
 end
